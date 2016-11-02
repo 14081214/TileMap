14c9aafef0df6e91ad48aa00a509d565d5147190
Ellali <lyh3109@sina.cn>
2016-11-02 17:55:41 +0800

    Finish Homework 6

--- src/Main.ts
+++ src/Main.ts
@@ -129,85 +129,6 @@ class Main extends egret.DisplayObjectContainer {
     private movingTime = 32;
     private gridSize = 64;
     private thePath = 0;
-
-    class Player{
-    public PlayerBitmap : egret.Bitmap;
-    private ifIdle : boolean;
-    private ifWalk : boolean;
-    private GoRight : boolean = false;
-    private GoLeft : boolean = false;
-    private IdleOrWalkStateMachine : StateMachine;
-    private LeftOrRightStateMachine : StateMachine;
-
-    constructor(){
-        this.PlayerBitmap = new egret.Bitmap();
-        this.PlayerBitmap.width = 49;
-        this.PlayerBitmap.height = 64;
-        // this.PlayerBitmap.anchorOffsetX = 2 * this.PlayerBitmap.width / 3;
-        // this.PlayerBitmap.anchorOffsetY = this.PlayerBitmap.height;
-        this.ifIdle = true;
-        this.ifWalk = false;
-        this.IdleOrWalkStateMachine = new StateMachine();
-        this.LeftOrRightStateMachine = new StateMachine();
-
-    }
-
-    public SetPlayerBitmap(picture:egret.Bitmap){
-        this.PlayerBitmap = picture;
-    }
-
-
-    public SetIdle(set : boolean){
-        this.ifIdle = set;
-    }
-
-    public GetIfIdle(): boolean{
-        return this.ifIdle;
-    }
-
-    public SetWalk(set : boolean){
-        this.ifWalk = set;
-    }
-
-    public GetIfWalk(): boolean{
-        return this.ifWalk
-    }
-
-    public SetGoRight(){
-        this.GoRight = true;
-        this.GoLeft = false;
-    }
-
-    public GetIfGoRight(): boolean{
-        return this.GoRight;
-    }
-
-    public SetGoLeft(){
-        this.GoLeft = true;
-        this.GoRight = false;
-    }
-
-    public GetIfGoLeft() : boolean{
-        return this.GoLeft;
-    }
-
-    private createBitmapByName(name:string):egret.Bitmap {
-        var result = new egret.Bitmap();
-        var texture:egret.Texture = RES.getRes(name);
-        result.texture = texture;
-        return result;
-    }
-
-    public SetState(e : State , _tmain : Main){
-        this.IdleOrWalkStateMachine.setState(e,_tmain);
-    }
-
-    public SetRightOrLeftState(e : State , _tmain : Main){
-        this.LeftOrRightStateMachine.setState(e,_tmain);
-    }
-
-
-}
     
     /**
      * 创建游戏场景
@@ -423,6 +344,86 @@ class Main extends egret.DisplayObjectContainer {
         
         FramePlus();
     }
+
+}
+
+
+class Player{
+    public PlayerBitmap : egret.Bitmap;
+    private ifIdle : boolean;
+    private ifWalk : boolean;
+    private GoRight : boolean = false;
+    private GoLeft : boolean = false;
+    private IdleOrWalkStateMachine : StateMachine;
+    private LeftOrRightStateMachine : StateMachine;
+
+    constructor(){
+        this.PlayerBitmap = new egret.Bitmap();
+        this.PlayerBitmap.width = 49;
+        this.PlayerBitmap.height = 64;
+        // this.PlayerBitmap.anchorOffsetX = 2 * this.PlayerBitmap.width / 3;
+        // this.PlayerBitmap.anchorOffsetY = this.PlayerBitmap.height;
+        this.ifIdle = true;
+        this.ifWalk = false;
+        this.IdleOrWalkStateMachine = new StateMachine();
+        this.LeftOrRightStateMachine = new StateMachine();
+
+    }
+
+    public SetPlayerBitmap(picture:egret.Bitmap){
+        this.PlayerBitmap = picture;
+    }
+
+
+    public SetIdle(set : boolean){
+        this.ifIdle = set;
+    }
+
+    public GetIfIdle(): boolean{
+        return this.ifIdle;
+    }
+
+    public SetWalk(set : boolean){
+        this.ifWalk = set;
+    }
+
+    public GetIfWalk(): boolean{
+        return this.ifWalk
+    }
+
+    public SetGoRight(){
+        this.GoRight = true;
+        this.GoLeft = false;
+    }
+
+    public GetIfGoRight(): boolean{
+        return this.GoRight;
+    }
+
+    public SetGoLeft(){
+        this.GoLeft = true;
+        this.GoRight = false;
+    }
+
+    public GetIfGoLeft() : boolean{
+        return this.GoLeft;
+    }
+
+    private createBitmapByName(name:string):egret.Bitmap {
+        var result = new egret.Bitmap();
+        var texture:egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+
+    public SetState(e : State , _tmain : Main){
+        this.IdleOrWalkStateMachine.setState(e,_tmain);
+    }
+
+    public SetRightOrLeftState(e : State , _tmain : Main){
+        this.LeftOrRightStateMachine.setState(e,_tmain);
+    }
+
 
 }
 
